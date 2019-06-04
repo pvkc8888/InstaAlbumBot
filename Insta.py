d45b6d462ad7dfeb7f30e31cb91b7f57f8a7e788
Vinay Pulivarthi <pvkc8888@yahoo.com>
2019-06-03 23:17:21 -0400

    Working on June 3 2019

--- Insta.py
+++ Insta.py
@@ -11,24 +11,16 @@ from selenium.webdriver.chrome.options import Options
 import sys
 import getpass
 
-images = re.compile(r'src="https://[-\w./"]*jpg')
+posts = re.compile(r'src="https://[/\S+/]*com')
 
 
 def OpenBrowser():
     driver = webdriver.Firefox()
-    # chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--window-size=1920x1080")
-    # chrome_driver = os.getcwd() + "\\chromedriver.exe"
-    # driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
     return driver
 
 
 def login(driver, username, password):
     driver.get('https://www.instagram.com/accounts/login/')
-    # time.sleep(3)
-    # login_button = driver.find_element(By.XPATH, '//a[@href = "/accounts/login/"]')
-    # login_button.click()
     time.sleep(2)
     username_elem = driver.find_element_by_xpath('//*[@name="username"]')
     username_elem.clear()
@@ -39,24 +31,28 @@ def login(driver, username, password):
     time.sleep(2)
 
 
-def LoadAccount(account_name):
+def GetAccount(account_name):
     driver.get('https://www.instagram.com/' + account_name)
     scroll_pause = 1
     last_height = driver.execute_script("return document.body.scrollHeight")
     big_list = []
+    test = time.time()
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(scroll_pause)
         new_height = driver.execute_script("return document.body.scrollHeight")
         last_height = new_height
         html_source = driver.page_source
-        pics = re.findall(images, html_source)
+        pics = re.findall(posts, html_source)
         for item in pics:
-            if item not in big_list:
+            if item[5:] not in big_list:
                 big_list.append(item[5:])
-        if new_height == last_height:
+                print(big_list)
+        # if new_height == last_height:
+        #     break
+        if time.time() - test > 120:
             break
-    print(big_list)
+    print(len(big_list))
     return big_list
 
 
@@ -69,21 +65,13 @@ def SaveImages(big_list):
         if count != len(big_list) - 1:
             urllib.request.urlretrieve(item, account_name + '\\' + str(count + 1) + '.jpg')
         count -= 1
-    # shutil.make_archive(account_name, 'zip', account_name)
 
 
 if __name__ == "__main__":
-    driver = OpenBrowser()
     account_name = str(sys.argv[1])
-    big_list = LoadAccount(account_name)
-    if len(big_list) == 1:
-        response = input('Its a private account, do you want to download with your credentials?')
-        if response == 'y':
-            username = input('Enter username: ')
-            password = getpass.getpass('Enter password: ')
-            login(driver, username, password)
-            big_list = LoadAccount(account_name)
-        else:
-            print('Sorry Creep')
-            exit()
+    username = input('Enter username: ')
+    password = getpass.getpass('Enter password: ')
+    driver = OpenBrowser()
+    login(driver, username, password)
+    big_list = GetAccount(account_name)
     SaveImages(big_list)
